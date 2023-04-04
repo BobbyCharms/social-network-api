8e6feb5ed284e4ba914a66a78e37162b78df3285
BobbyCharms <robertcharming@Gmail.com>
2023-04-04 09:49:51 -0400

    adds reaction schema for subdocument schema in thoughts model

--- models/reaction.js
+++ models/reaction.js
@@ -1,1 +1,36 @@
 const { Schema, model } = require('mongoose')
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.Object(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+reactionSchema.virtual.("createdAt").get(function() {
+    return (this.createdAt.toLocaleTimeString() + "on" + this.createdAt.toLocaleTimeString());
+});
+
+module.exports = reactionSchema;
