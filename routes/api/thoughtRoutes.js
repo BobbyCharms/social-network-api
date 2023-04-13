3147f917debdcf31ec1431419a21466b75aa78d8
BobbyCharms <robertcharming@Gmail.com>
2023-04-13 11:19:21 -0400

    adds the thought routes referencing the thoughts controller

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,9 +1,21 @@
 const router = require('express').Router();
 
 const {
-    getThoughts, 
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction, 
 } = require('../../controllers/thoughtsController.js');
 
 router.route('/').get(getThoughts).post(createThought);
+
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(addReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
 
 module.exports = router;
